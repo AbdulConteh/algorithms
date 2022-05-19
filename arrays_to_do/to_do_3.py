3b1f3791df3ea0d78dd6f98ffa08391cf7ec5238
AbdulConteh <abdulrahmanwc@gmail.com>
2022-05-18 23:57:03 -0400

    array practice

---
+++ arrays_to_do/to_do_3.py
@@ -0,0 +1,11 @@
+# removing negatives 
+arr = [-1,-5,6,7,8,-8]
+new_arr = [ i for i in arr if i >= 0]
+
+print(new_arr)
+
+# second to last 
+arr = [42, "true", 4, "kate", 7]
+print(arr[-2])
+
+# second largest 
